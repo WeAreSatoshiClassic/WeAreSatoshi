c58057db79669c2a9e3e90e516b442c7eee5ec68
blbnguyen <blbnguyen@users.noreply.github.com>
2018-09-18 09:46:08 -0400

    Correct dev fund calc

--- src/main.h
+++ src/main.h
@@ -29,7 +29,7 @@ class CNode;
 
 static const unsigned int WSX_2_FORK = 802000;
 static const int64_t COIN_REWARD_STAGE_FORK = 0.15 * COIN; // 15% per year normal interest
-static const int64_t WSX_DEV_PERCENT = 0.1; //10% of the inflation
+static const float_t WSX_DEV_PERCENT = 0.1; //10% of the inflation
 
 static const unsigned int MAX_BLOCK_SIZE = 1000000;
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
