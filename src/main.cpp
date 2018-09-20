fe7777332e0df6872fe3a3772374e6e41bce896c
blockzerodev <konez@protonmail.com>
2018-09-21 00:06:17 +0200

    fix pos

--- src/main.cpp
+++ src/main.cpp
@@ -1041,7 +1041,9 @@ int64_t GetProofOfStakeReward(int nHeight, int64_t nCoinAge, int64_t nFees)
     if (nHeight >= WSX_2_FORK)
         nSubsidy = nCoinAge * nRewardCoinYear * 33 / (365 * 33 + 8);
     else
-        nSubsidy = nCoinAge * nRewardCoinYear / 365 / COIN;
+        nSubsidy = nCoinAge * nRewardCoinYear / 365;
+
+    nSubsidy /= COIN;
 
     if (fDebug && GetBoolArg("-printcreation"))
         printf("GetProofOfStakeReward(): create=%s nCoinAge=%"PRId64" nRewardCoinYear=%"PRId64"\n", FormatMoney(nSubsidy).c_str(), nCoinAge, nRewardCoinYear);
