96afb20d9229ea80322aac5fa94564b69684f831
blockzerodev <konez@protonmail.com>
2018-09-25 10:09:37 +0200

    updated protocol version

--- src/version.h
+++ src/version.h
@@ -30,11 +30,11 @@ static const int DATABASE_VERSION = 1;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 20007;
+static const int PROTOCOL_VERSION = 20077;
 
 // earlier versions not supported and are disconnected
-static const int MIN_PROTO_VERSION = 20076;
-static const int MIN_PROTO_VERSION_FORKV2 = 20007;
+static const int MIN_PROTO_VERSION = 20077;
+static const int MIN_PROTO_VERSION_FORKV2 = 20077;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
